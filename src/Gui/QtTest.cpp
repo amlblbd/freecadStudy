445c44fdab0d874b9cdf719c6234ffc025acd2f9
amlblbd <1752437265@qq.com>
2026-01-16 10:32:17 +0800

    20260116 qt配置成功

--- src/Gui/QtTest.cpp
+++ src/Gui/QtTest.cpp
@@ -1,15 +1,25 @@
 #include <QApplication>
 #include <QByteArray>
 #include <QCoreApplication>
+#include <QDebug>
 #include <QLabel>
 #include <QTimer>
+#include <QtPlugin>
+
+class QXcbIntegrationPlugin;
+Q_IMPORT_PLUGIN(QXcbIntegrationPlugin)
 
 int main(int argc, char** argv) {
     if (qEnvironmentVariableIsEmpty("DISPLAY") && qEnvironmentVariableIsEmpty("WAYLAND_DISPLAY")) {
         qputenv("QT_QPA_PLATFORM", QByteArray("offscreen"));
+    } else if (qEnvironmentVariableIsEmpty("QT_QPA_PLATFORM")) {
+        qputenv("QT_QPA_PLATFORM", QByteArray("xcb"));
     }
 
     QApplication app(argc, argv);
+
+    qInfo() << "Qt runtime version:" << qVersion();
+    qInfo() << "Qt compile-time version:" << QT_VERSION_STR;
 
     QLabel label("Qt 6.6.2 OK");
     label.resize(320, 80);
